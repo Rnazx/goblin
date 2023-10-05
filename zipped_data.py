3559155459409b8deebe47d7100629b0cb1ab701
Rion <riong.nazareth@niser.ac.in>
2023-10-05 13:57:48 +0530

    change the df itself

--- zipped_data.py
+++ zipped_data.py
@@ -39,57 +39,47 @@ with open(current_directory+'\switches.in', 'r') as FH:
 print('Succesfully read the parameters and switches')
 
 #choose galaxy name in the next line to choose the galaxy
-# with open(current_directory+ '\data\data_interpolated.csv', 'rb') as f:
-#     data = pickle.load(f)
 os.chdir(current_directory+'\data')
-data = (pd.read_csv('data_interpolated.csv')) #.T
-#data = data.drop(data.index[0])
-#use this for m31 and m33
-# kpc_r = np.array(data["Radius(kpc)"])
-#print(tuple((data.values.T)[1:]))
-kpc_r, dat_sigmatot, dat_sigmaHI,dat_sigmaH2, dat_q, dat_omega, dat_sigmasfr, T= tuple((data.values.T))
-# print(kpc_r)
+data = (pd.read_csv('data_interpolated.csv')) 
+
+kpc_r = data.iloc[:, 0].values
 # finding total gas density
 # adds HI and H2 data based on switch 
 if switch['incl_moldat'] == 'Yes': #this is set to 'NO' in switches.in file
-    dat_sigma = dat_sigmaHI + dat_sigmaH2
-else:
-    dat_sigma = dat_sigmaHI
+    data[data.columns[2]] = data.iloc[:, 2] + data.iloc[:, 3]
 
+data.drop(data.columns[3], axis=1, inplace=True)
+data.rename(columns={data.columns[2]: '\sigma_gas'}, inplace=True)
 #use this for m51 and 6946
 #difference as here we have to mandatorily consider molecular gas density
 # kpc_r, dat_sigmatot, dat_sigma, dat_q, dat_omega, dat_sigmasfr, T= data
 r = kpc_r.size  # common radius of the interpolated data
 
-dat_sigmagas = (3*params['mu'])*dat_sigma/(4-params['mu']) #mu= 14/11 set in parameters.in file
-
+#mu= 14/11 set in parameters.in file
+data['\sigma_gas']*= (3*params['mu'])/(4-params['mu'])
 #####################################################################################################################
 
 #####################################################################################################################
 #to apply kennicut-schmidt relation
 ks_exp = params['ks_exp']
-ks_const = (dat_sigmasfr/(dat_sigma)**(ks_exp)).mean()
+ks_const = (data['sigma_sfr']/(data['\sigma_gas'])**(ks_exp)).mean()
 
 ks_split = switch['force_kennicut_scmidt'].split() #currently this is set to 'No, sigmadata'
 if ks_split[0] == 'Yes':
     if ks_split[1] == 'sigmasfrdata':
-        dat_sigma = (dat_sigmasfr/ks_const)**(1/ks_exp)
+        data['\sigma_gas'] = (data['sigma_sfr']/ks_const)**(1/ks_exp)
     else:
-        dat_sigmasfr = ks_const*(dat_sigma)**(ks_exp)
+        data['sigma_sfr'] = ks_const*(data['\sigma_gas'])**(ks_exp)
 #####################################################################################################################
+params_df = pd.DataFrame({key: [value] * r for key, value in params.items() if key != 'ks_exp'})
+data_params = data.join(params_df)
 
-#changes based on galaxy and quality of fit
-zet = params['\zeta']*np.ones(r)
-psi = params['\psi']*np.ones(r)
-bet = params[r'\beta']*np.ones(r)
-
-ca = params[r'C_\alpha']*np.ones(r)
-rk = params['R_\kappa']*np.ones(r)
-mu = params['mu']*np.ones(r)
-
+data_listoftup = list(data_params[data_params.columns[1:]].to_records(index=False))
 # zip function makes array of corresponding elements of each array passed into it
-data_pass = kpc_r, list(zip(dat_sigmatot, dat_sigmagas, dat_sigmasfr,
-                 dat_q, dat_omega, zet, T, psi, bet, ca, rk, mu))
+data_pass = kpc_r, data_listoftup
+# (data_pass[1]).sort()
+# (list(data_params[data_params.columns[1:]].to_records(index=False))).sort()
+#if data_pass[1] == list(data_params[data_params.columns[1:]].to_records(index=False)): print('You are a God rion')
 
 #dont change this pickle file name
 with open(current_directory+ '\data\zip_data.pickle', 'wb') as f:
