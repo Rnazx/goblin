838b2b0aac9022185036093e8008a6c2a9b39c14
Rion <riong.nazareth@niser.ac.in>
2023-10-19 16:23:52 +0530

    associated changes

--- data/data_selector.sh
+++
@@ -1,7 +0,0 @@
-printf "Choose from the following list of galaxies \n"
-printf "1. M31   ||   2. M33   ||   3. M51   ||   4. NGC6946   \n"
-read galname
-printf "You have selected $galname \n"  
-printf "Running interpolation for galaxy $galname \n"
-cd $galname"_data"
-python "data_"$galname".py"
