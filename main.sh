8e305140c517273e47af65ca56411c855b9adbc8
Rion <riong.nazareth@niser.ac.in>
2023-10-19 16:20:22 +0530

    take into account source

--- main.sh
+++ main.sh
@@ -1,21 +1,27 @@
+#!/bin/bash
+source config.sh
+
 # Put the data in a usable format
 cd data
-python data_correction.py
-python data_common.py
+python data_common.py 
 
 #Solve all the expressions in terms of the scale height h
 cd ../expressions
 # python turbulence_expressions.py
 # python magnetic_expressions.py
 
-cd ..
+cd ../src
 #Change the data into the preffered format for Sympy
 python zipped_data.py
 #Find the root and solve for all quantities
-python get_magnetic_observables.py
+python model_predictions.py
 
 #If there is a change in the model run this to fing new scaling relations
 #python scale_exponents.py
 
 #Error aprroximation
 #python error_approximation.py
+
+#Plotting
+python plot_generator.py
+
