9811624270cd01b176f6a1286460d81cfde1c053
Rion <riong.nazareth@niser.ac.in>
2024-10-15 09:31:57 +0530

    changed main.sh back

--- main.sh
+++ main.sh
@@ -3,18 +3,13 @@ source config.sh
 
 # Put the data in a usable format
 cd data
-<<<<<<< HEAD
-# python data_common.py 
-# python vel_disp.py
-=======
 python data_common.py 
 python velocity_dispersion.py
->>>>>>> 4eee51b97cd7755aec848854d9c4f0e23e79c2cd
 
 # #Solve all the expressions in terms of the scale height h
 # cd ../expressions
-# python turbulence_expressions.py
-# python magnetic_expressions.py
+python turbulence_expressions.py
+python magnetic_expressions.py
 
 cd ../src
 #Change the data into the preffered format for Sympy
